f096cdedef4c77f7e2a0121b975d81381fac3442
Actat <akiotoshima@gmail.com>
2018-09-06 21:52:44 +0900

    main activity

--- app/src/main/java/com/example/actat/simplepaint/MainActivity.java
+++ app/src/main/java/com/example/actat/simplepaint/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.actat.simplepaint;
 
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CanvasView cv = (CanvasView) findViewById(R.id.canvasview);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cv.reset();
+            }
+        });
     }
 }
